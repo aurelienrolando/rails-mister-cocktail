2b6f05fa05a7f06e028061347ed249d32d7b87f7
Aurelien Rolando <aurelien.rolando@gmail.com>
2020-05-15 12:53:18 +0200

    Destroy method added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :cocktails do
-    ressources :doses
+    resources :doses, except: [:destroy]
+  end
+
+  resources :doses, only: [:destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
